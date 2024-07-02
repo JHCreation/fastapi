6aed4ac8ae1447201b685c8547089f4ec542219a
jhceation <rainymanson@gmail.com>
2024-07-02 17:27:13 +0900

    17:27

--- conn.py
+++ conn.py
@@ -8,8 +8,9 @@ PASSWORD= os.environ.get('DB_PASSWORD')
 HOST= os.environ.get('DB_HOST')
 PORT= os.environ.get('DB_PORT')
 DBNAME= os.environ.get('DB_NAME')
-# DB_URL= os.environ.get('DB_URL')
-# DB_URL_ASYNC= os.environ.get('DB_URL_ASYNC')
+DB_URL= os.environ.get('DB_URL')
+DB_URL_ASYNC= os.environ.get('DB_URL_ASYNC')
+
 
 DATABASE_URL = f"mysql+pymysql://{USERNAME}:{quote(PASSWORD)}@{HOST}:{PORT}/{DBNAME}"
 DATABASE_URL_ASYNC = f"mysql+aiomysql://{USERNAME}:{quote(PASSWORD)}@{HOST}:{PORT}/{DBNAME}"
