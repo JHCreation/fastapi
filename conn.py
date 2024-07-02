7eecf06795f41403f0a65cb68d6946f8ee321cc8
jhceation <rainymanson@gmail.com>
2024-07-02 19:22:23 +0900

    19:22

--- conn.py
+++ conn.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 USERNAME= os.environ.get('DB_USERNAME')
 PASSWORD= os.environ.get('DB_PASSWORD')
+ENCORD_PW= quote_plus(PASSWORD)
 HOST= os.environ.get('DB_HOST')
 PORT= os.environ.get('DB_PORT')
 DBNAME= os.environ.get('DB_NAME')
@@ -12,7 +13,7 @@ DB_URL= os.environ.get('DB_URL')
 DB_URL_ASYNC= os.environ.get('DB_URL_ASYNC')
 
 
-DATABASE_URL = f"mysql+pymysql://{USERNAME}:{quote_plus(PASSWORD)}@{HOST}:{PORT}/{DBNAME}"
-DATABASE_URL_ASYNC = f"mysql+aiomysql://{USERNAME}:{quote_plus(PASSWORD)}@{HOST}:{PORT}/{DBNAME}"
+DATABASE_URL = f"mysql+pymysql://{USERNAME}:{ENCORD_PW}@{HOST}:{PORT}/{DBNAME}"
+DATABASE_URL_ASYNC = f"mysql+aiomysql://{USERNAME}:{ENCORD_PW}@{HOST}:{PORT}/{DBNAME}"
 # DATABASE_URL = DB_URL
 # DATABASE_URL_ASYNC = DB_URL_ASYNC
